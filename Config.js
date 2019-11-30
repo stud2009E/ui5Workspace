c9cffa25c4e3e6934f413ea970dee8aaef1c3091
Виноградов Дмитрий Сергеевич <Vinogradov.Dm.S@sberbank.ru>
2019-11-30 22:18:39 +0300

    Add Config for WorkSpace

--- Config.js
+++ Config.js
@@ -2,5 +2,14 @@ module.exports = {
     "localSDKPath": "/Users/a18201923/Documents/SAPUI5 SDK/sapui5-sdk-1.60.7/",
     "server": {
         "port": 8000
-    }
+    },
+    "libs": [{
+        "bspContainer": "ZSPL_LIB",
+        "name": "sb.fiori.lib.spl",
+        "active": true
+    }],
+    "apps": [{
+        "bspContainer": "ZSPL_FPI",
+        "active": true
+    }]
 }
