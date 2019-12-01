50c9bd30a83831790353e31a2842dbd64da3dd0d
Виноградов Дмитрий Сергеевич <Vinogradov.Dm.S@sberbank.ru>
2019-12-01 13:46:52 +0300

    Added Proxy for SDD System : command - grunt prod

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,9 +50,44 @@ module.exports = function (grunt) {
         grunt.config.set("connect.server.proxies", proxies);
     });
 
+    grunt.registerTask("setProdProxies", function () {
+        let proxies = [];
+        Config.libs.forEach(function (oLib) {
+            let proxy = {
+                context: "/sap/bc/ui5_ui5/sap/" + oLib.bspContainer,
+                host: "localhost",
+                port: Config.server.port,
+                https: false,
+                rewrite: {}
+            };
+            proxy.rewrite["^/sap/bc/ui5_ui5/sap/" + oLib.bspContainer + "/"] = "/ui/libs/" + oLib.bspContainer + "/src/" + oLib.name.replace(/[.]/g, "/") + "/";
+            proxies.push(proxy);
+        });
+        let proxySDD = {
+            context: "/sap/opu/odata/SAP/",
+            host: "sap-sdd001.sigma.sbrf.ru",
+            port: 8001,
+            secure: false,
+            https: true,
+            headers: {
+                'Authorization': 'Basic cHJlb2JyYXpoZW5zOnN2NGdsNk5N'
+            }
+        };
+        proxies.push(proxySDD);
+        grunt.config.set("connect.server.proxies", proxies);
+    });
+
     grunt.registerTask("serve", function () {
         grunt.task.run([
             "setProxies",
+            "configureProxies:server",
+            "openui5_connect:server"
+        ]);
+    });
+
+    grunt.registerTask("prod", function () {
+        grunt.task.run([
+            "setProdProxies",
             "configureProxies:server",
             "openui5_connect:server"
         ]);
