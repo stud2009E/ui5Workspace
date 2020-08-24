0ee5a28ad359bea5089aa757b5cab78d67fc0101
Пидалин Александр Борисович <18547995@sigma.sbrf.ru>
2020-08-24 18:26:33 +0300

    remove unused tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,22 +1,9 @@
 module.exports = function(grunt){
-
 	grunt.loadTasks("tasks");
 
 	grunt.registerTask("dev", "public: start dev server", [
 		"shellConfigCollect",
 		"flpIndexBuild",
 		"serve"
-	]);
-
-
-	grunt.registerTask("buildExcel", "public: build excel metadata.xlsx based on metadata.xml", [
-		"shellConfigCollect",
-		"excelMaket"
-	]);
-
-
-	grunt.registerTask("parseExcel", "public: build mockdata jsons from metadata.xlsx", [
-		"shellConfigCollect",
-		"excel2json"
 	]);
 };
