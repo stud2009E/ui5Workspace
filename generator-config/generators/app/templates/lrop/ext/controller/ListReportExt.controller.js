32ddb419e3fbee39f5831d92dc945539fa7fa1fd
18547995 <18547995@sigma.sbrf.ru>
2021-10-25 13:00:47 +0300

    [FIX] add module definition for sap.ui.controller

--- generator-config/generators/app/templates/lrop/ext/controller/ListReportExt.controller.js
+++ generator-config/generators/app/templates/lrop/ext/controller/ListReportExt.controller.js
@@ -1,7 +1,15 @@
-sap.ui.controller("<%= nmsp %>.ext.controller.ListReportExt", {
-	onInit: function(){
-		
-		//here goes your code
+sap.ui.define([
+	"sb/fiori/lib/spl/util/CommonUtils",
+	"sb/fiori/lib/spl/util/GridTableExtension"
+], function(
+	CommonUtils
+){
+	"use strict";
+	
+	sap.ui.controller("<%= nmsp %>.ext.controller.ListReportExt", {
+		onInit: function(){
+			CommonUtils.inject(this);
 
-	}
+		}
+	});
 });
