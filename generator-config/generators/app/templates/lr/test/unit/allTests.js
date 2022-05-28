d85d0463286ea6e99d3053cda5a08988d878e1aa
unknown <stud2009@yandex.ru>
2022-05-29 01:49:59 +0300

    refact: remove unused template

--- generator-config/generators/app/templates/lr/test/unit/allTests.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define([
-	"<%= path %>/test/unit/example/test"
-], function(){
-
-	"use strict";
-
-});
--- generator-config/generators/app/templates/op/ext/controller/ListReportExt.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.controller("<%= nmsp %>.ext.controller.ListReportExt", {
-	onInit: function(){
-		
-		throw new Error("access denied");
-
-	}
-});
